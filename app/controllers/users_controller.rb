3b32254611b6e1e920324e686b0d3f294889ad1d
ユーザ名 <n.kouki0619@gmail.com>
2022-02-01 08:53:35 +0900

    ユーザー詳細、編集2
    ユーザーの詳細ページ、編集ページの作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,4 +13,16 @@ class UsersController < ApplicationController
     #user一人のidを見つけて@userに代入
     @user = User.find(params[:id])
   end
+
+  def update
+    #ユーザー情報を更新しユーザー詳細画面に遷移
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    redirect_to user_path(@user)
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:username, :email, :profile, :profile_image)
+  end
 end
--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,6 @@
 #     https: false
 #   )
 # end
+
+
+Refile.secret_key = '19dfa9b5329d4510ba69ad618c42f58c616c117776c3a4e8958637f6e0b290106545f9d30abc99781a4baf1d8f18878fc4f432598f5103ecc4ba792377423aa6'
